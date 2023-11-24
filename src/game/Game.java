86638e14bbfad89fca8fd107496fd1f22126772f
AIT TR Student <Romano305@gmail.com>
2023-11-24 10:07:09 +0100

    renamed

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,7 +2,7 @@ package game;
 
 import java.util.Scanner;
 
-public class game {
+public class Game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
