baa3c8b80bbd99edcc7e228382c78064094e6d6c
AIT TR Student <Romano305@gmail.com>
2023-11-26 22:47:25 +0100

    new file Fibonacci

---
+++ src/Nov_24_11_23/Fibonacci.java
@@ -0,0 +1,19 @@
+package Nov_24_11_23;
+
+public class Fibonacci {
+    public static void main(String[] args){
+
+        int n = 100;
+        int y = 1;
+
+        for (int i = 0; i <= 100;){
+           int sum = i + y;
+            System.out.print(sum);
+            y= i;
+            i = sum;
+
+        }
+        }
+
+    }
+
