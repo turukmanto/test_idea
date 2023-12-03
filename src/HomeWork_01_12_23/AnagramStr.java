287a4ab842c8e7f67f3fe4a9d582b1938da40662
AIT TR Student <Romano305@gmail.com>
2023-12-03 01:19:45 +0100

    letters check

--- src/HomeWork_01_12_23/AnagramStr.java
+++ src/HomeWork_01_12_23/AnagramStr.java
@@ -1,10 +1,12 @@
 package HomeWork_01_12_23;
 
+import java.util.Arrays;
+
 public class AnagramStr {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        //Написать метод, принимающий две строки и проверяющий является ли одна строка анграммой другой.Метод
+        //Написать метод, принимающий две строки и проверяющий является ли одна строка анаграммой другой.Метод
         //возвращает boolean
         //abcd  - bcda acbd - анаграммы
         //abcd -    acb  abkc - не анаграммы
@@ -13,6 +15,31 @@ public class AnagramStr {
         //toCharArray
         //Arrays.sort
 
+        //System.out.println(checkLetters("lipa","apil"));
 
-    }
-}
+
+
+    //}
+       // public static Boolean checkLetters(String str1,String str2){
+
+        String str1 = "pila";
+        String str2 = "alip";
+
+
+        char[] newStr1 = str1.toCharArray();
+        char[] newStr2 = str2.toCharArray();
+
+        Arrays.sort(newStr1);
+        Arrays.sort(newStr2);
+
+        //System.out.println(Arrays.toString(newStr1));
+        //System.out.println(Arrays.toString(newStr2));
+         System.out.println(Arrays.toString(newStr1).equals(Arrays.toString(newStr2)));
+
+
+        }
+        }
+
+
+
+
